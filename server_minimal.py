94e2d39011df5bc463961d25e37a9aa016864c3b
Jonathan <jonathan@latos.ai>
2021-09-07 08:55:48 -0300

    changes to asynchronous library

--- server_minimal.py
+++ server_minimal.py
@@ -1,74 +1,47 @@
+import logging
+import asyncio
 import sys
-
 sys.path.insert(0, "..")
-import time
-import numpy as np
-from opcua import ua, Server
-import datetime as dt
+
+from asyncua import ua, Server
+from asyncua.common.methods import uamethod
 
 
-if __name__ == "__main__":
+
+@uamethod
+def func(parent, value):
+    return value * 2
+
+
+async def main():
+    _logger = logging.getLogger('asyncua')
     # setup our server
     server = Server()
-    server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
+    await server.init()
+    server.set_endpoint('opc.tcp://0.0.0.0:4840/freeopcua/server/')
 
     # setup our own namespace, not really necessary but should as spec
-    uri = "http://examples.freeopcua.github.io"
-    idx = server.register_namespace(uri)
+    uri = 'http://examples.freeopcua.github.io'
+    idx = await server.register_namespace(uri)
 
-    device = server.nodes.objects.add_object(idx, "Simulator")
-    name = device.add_variable(idx, "serialNumber", "test")
-    name.set_writable()
-
-    variables = {}
-
-    # random variables
-    n_variables = 10
-    variables['random'] = {}
-    random_variables = device.add_object(idx, "RandomVariables")
-    for n_variable in range(n_variables):
-        var_name = 'random_' + str(n_variable)
-        variables['random'][var_name] = random_variables.add_variable(idx, var_name, 0)
-
-    # random cumulate
-    n_variables = 10
-    variables['random_cumulate'] = {}
-    random_variables = device.add_object(idx, "RandomCumulateVariables")
-    for n_variable in range(n_variables):
-        var_name = 'random_cumulate' + str(n_variable)
-        variables['random'][var_name] = random_variables.add_variable(idx, var_name, 0)
-    
-    # periodic variables
-    variables['periodic'] = {}
-    periodic_variables = device.add_object(idx, "PeriodicVariables")
-    variables['periodic']['sin'] = periodic_variables.add_variable(idx, 'sin', 0)
-    variables['periodic']['step'] = periodic_variables.add_variable(idx, 'step', 0)
-
-    # starting!
-    server.start()
-
-    try:
-        count = 0
+    # populating our address space
+    # server.nodes, contains links to very common nodes like objects and root
+    myobj = await server.nodes.objects.add_object(idx, 'MyObject')
+    myvar = await myobj.add_variable(idx, 'MyVariable', 6.7)
+    # Set MyVariable to be writable by clients
+    await myvar.set_writable()
+    await server.nodes.objects.add_method(ua.NodeId('ServerMethod', 2), ua.QualifiedName('ServerMethod', 2), func, [ua.VariantType.Int64], [ua.VariantType.Int64])
+    _logger.info('Starting server!')
+    async with server:
         while True:
-            time.sleep(1)
-
-            # random variables
-            for variable in variables['random'].values():
-                variable.set_value(np.random.randn(1)[0])
-                
-    
-            # random cumulate
-            for variable in variables['random_cumulate'].values():
-                old = variable.get_value()
-                variable.set_value(np.random.randn(1)[0] + old)
+            await asyncio.sleep(1)
+            new_val = await myvar.get_value() + 0.1
+            _logger.info('Set value of %s to %.1f', myvar, new_val)
+            await myvar.write_value(new_val)
 
 
-            # periodic
-            sec = dt.datetime.now().second
-            sin_value = np.sin(sec * np.pi / 30)
-            variables['periodic']['sin'].set_value(sin_value)
-            variables['periodic']['step'].set_value(np.sign(sin_value))
+if __name__ == '__main__':
 
-    finally:
-        # close connection, remove subcsriptions, etc
-        server.stop()
+    logging.basicConfig(level=logging.DEBUG)
+
+    asyncio.run(main(), debug=True)
